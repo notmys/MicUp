c645ba06b43964aaa8dfd4656c8322ba0e96ace2
Mys (01xMYS) <mysontop.co@gmail.com>
2025-08-30 19:36:12 -0400

    Create users.lua

---
+++ admin/users.lua
@@ -0,0 +1,8 @@
+-- whitelist.lua
+whitelist = {
+    ["01xMYS"] = "owner",
+    ["01xSILENCE"] = "owner",
+    ["Strixxsy"] = "owner",
+    ["e2edeq"] = "CoOwner",
+    ["carmenanswerme"] = "owner",
+}
