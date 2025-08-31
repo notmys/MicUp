b4684464a0c9cdb8ff7417bfc4ec7f9ae0f402a9
Mys (01xMYS) <mysontop.co@gmail.com>
2025-08-30 20:36:18 -0400

    Update users.lua

--- admin/users.lua
+++ admin/users.lua
@@ -4,5 +4,9 @@ whitelist = {
     ["01xSILENCE"] = "owner",
     ["Strixxsy"] = "owner",
     ["e2edeq"] = "CoOwner",
+    ["jtyrjetahtersjrt"] = "CoOwner",
+    ["chip_xoxalt"] = "Admin",
+    ["FroggyWinks"] = "Admin",
+    ["Bondzo72"] = "Admin",
     ["carmenanswerme"] = "owner",
 }
