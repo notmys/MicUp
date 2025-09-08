94e87d226d90affc4c9a3b77d291b35f5243569f
Mys (01xMYS) <mysontop.co@gmail.com>
2025-09-08 05:22:27 -0400

    Create Nametags.lua

---
+++ api/Nametags.lua
@@ -0,0 +1,9 @@
+-- userlists.lua
+return {
+    Owners = {"01xMYS", "Strixxsy"},
+    CoOwner = {},
+    MysStaff = {},
+    Advertisers = {},
+    Helper = {},
+    Giveaway1 = {"Dupersigmadupe"}
+}
